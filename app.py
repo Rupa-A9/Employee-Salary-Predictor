d3350e092f383f3fbcc65c2728cde8f352a9fc4d
Rupasri Siripurapu <siripurapurupasri@gmail.com>
2025-07-21 21:00:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ if uploaded_file is not None:
 
     try:
         # Load the model
-        model = joblib.load("model.pkl")
+        model = joblib.load("model(1).pkl")
         st.success("✅ Model loaded successfully!")
 
         # Predict
